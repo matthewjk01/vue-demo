97256cf06adfb943fd5f0d55a98ed90e49b8a406
matthew <1059121369@qq.com>
2017-12-05 14:45:57 +0800

    add elementUi

--- vue-demo/src/components/HelloWorld.vue
+++ vue-demo/src/components/HelloWorld.vue
@@ -17,11 +17,15 @@
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
+    <el-button type="primary">点我</el-button>
   </div>
 </template>
 
 <script>
+import ElButton from "../../node_modules/element-ui/packages/button/src/button.vue";
+
 export default {
+  components: {ElButton},
   name: 'HelloWorld',
   data () {
     return {
